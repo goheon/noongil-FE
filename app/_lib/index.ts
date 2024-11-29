315060011b1cc40a0a87f8bd16530ca62dd72b1e
goheon <gohoney22@gmail.com>
2024-11-29 21:41:04 +0900

    feat: 네이버지도SDK 부착

--- app/_lib/index.ts
+++ app/_lib/index.ts
@@ -1,2 +1,3 @@
 export * from './queryClient'
 export * from './stores'
+export * from './hooks'
--- app/_types/props.d.ts
+++ app/_types/props.d.ts
@@ -1,9 +1,11 @@
+// 전시 상세 페이지 props
 export interface ExhibitionDetailPageProps {
   params: {
     exhibitionNo: string
   }
 }
 
+// 팝업 상세 페이지 props
 export interface PopupDetailPageProps {
   params: {
     popupNo: string
