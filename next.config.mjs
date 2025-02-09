88f1c97632032641941f542457fb5333c7cf7b77
mokyoungg <mokyoungg@gmail.com>
2025-02-09 11:06:04 +0900

    feat: update next config for images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@ const nextConfig = {
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
   },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+      },
+    ],
+  },
 }
 
 export default nextConfig
