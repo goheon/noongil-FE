6bdd003f40863794de2efdbebdf4c4c9e4ac6f57
mokyoungg <mokyoungg@gmail.com>
2025-02-14 14:05:04 +0900

    feat: define response data type

---
+++ app/_components/features/searchList/type.ts
@@ -0,0 +1,17 @@
+export interface ISearchListItem {
+  eventId: number
+  eventNm: string
+  eventCntn: string
+  eventAddr: string
+  operStatDt: string
+  operEndDt: string
+  operDttmCntn: string
+  ctgyId: string
+  ctgyNm: string
+  eventTypeCd: string
+  eventTypeNm: string
+  likeYn: string
+  viewNmvl: number
+  imageUrl: string
+  smallImageUrl: string
+}
