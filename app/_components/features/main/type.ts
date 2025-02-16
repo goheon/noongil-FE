ddcf93cd4bcc0d60524edb986f3a4717e479d83c
mokyoungg <mokyoungg@gmail.com>
2025-02-16 17:50:18 +0900

    feat: update main api response type

--- app/_components/features/main/type.ts
+++ app/_components/features/main/type.ts
@@ -1,17 +1,11 @@
 export interface IListItem {
   eventId: number
   eventNm: string
-  eventCntn: string
-  eventAddr: string
   operStatDt: string
   operEndDt: string
-  operDttmCntn: string
   ctgyId: string
-  ctgyNm: string
   eventTypeCd: string
-  eventTypeNm: string
   likeYn: string
-  viewNmvl: number
   imageUrl: string
   smallImageUrl: string
 }
