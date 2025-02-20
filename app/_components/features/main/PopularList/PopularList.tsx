82d87df07c7d13937a68d72f861aba27bc44ffff
mokyoungg <mokyoungg@gmail.com>
2025-02-20 19:04:53 +0900

    feat: update period list with components

--- app/_components/features/main/PopularList/PopularList.tsx
+++ app/_components/features/main/PopularList/PopularList.tsx
@@ -13,12 +13,11 @@ import { Pagination } from 'swiper/modules'
 
 import usePopularList from './usePopularList'
 import { IListItem } from '../type'
-import Image from 'next/image'
-import SampleImage from '@/public/free-img.jpg'
 import { useMemo } from 'react'
-import { formatDateRange } from '@/app/_utils/textFormatter'
-import Skeleton from 'react-loading-skeleton'
 import { EVENT_CATEGORY_MAP } from '../type'
+
+import PopularListItem from './PopularListItem'
+import SkeletonList from './SkeletonList'
 
 const cx = classNames.bind(styles)
 
@@ -52,45 +51,20 @@ const PopularList = (props: PopularListProps) => {
       </div>
 
       {isLoading ? (
-        <div className={cx('skeleton-box')}>
-          <div className={cx('skeleton-wrapper')}>
-            <Skeleton width={200} height={200} borderRadius={5} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-          </div>
-
-          <div className={cx('skeleton-wrapper')}>
-            <Skeleton width={200} height={200} borderRadius={5} />
-            <Skeleton width={200} />
-            <Skeleton width={200} />
-          </div>
-        </div>
+        <SkeletonList />
       ) : (
         <Swiper
           slidesPerView={'auto'}
           modules={[Pagination]}
-          className={cx('swiper')}
+          className={cx('list')}
         >
           {popularList.length > 0 &&
             popularList.map((data: IListItem, idx: number) => (
-              <SwiperSlide key={data.eventId} className={cx('list-item')}>
-                <Image
-                  className={cx('image')}
-                  src={data.imageUrl || SampleImage}
-                  alt="image"
-                  width={200}
-                  height={200}
-                />
-                <div className={cx('info-section')}>
-                  <div className={cx('rank')}>{idx + 1}</div>
-
-                  <div className={cx('info')}>
-                    <div className={cx('title')}>{data.eventNm}</div>
-                    <div className={cx('event-date')}>
-                      {formatDateRange(data.operStatDt, data.operEndDt)}
-                    </div>
-                  </div>
-                </div>
+              <SwiperSlide
+                className={cx('list-item-wrapper')}
+                key={data.eventId}
+              >
+                <PopularListItem data={data} idx={idx} />
               </SwiperSlide>
             ))}
         </Swiper>
