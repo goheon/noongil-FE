3973f355fd05133cd23629948b8f7f50c1a61548
mokyoungg <mokyoungg@gmail.com>
2025-03-05 20:19:40 +0900

    feat: add naver map client key

--- app/_lib/constants.ts
+++ app/_lib/constants.ts
@@ -3,6 +3,7 @@ type Environment = keyof typeof API_BASE_URL_LIST
 const ENV: Environment = process.env.NODE_ENV
 
 const NAVER_MAP_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID
+const NAVER_MAP_CLIENT_KEY = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_KEY
 
 const API_BASE_URL_LIST = {
   production: 'asd',
@@ -17,6 +18,7 @@ const VALID_CATEGORIES = ['popup', 'exhibition']
 const config = {
   ENV,
   NAVER_MAP_CLIENT_ID,
+  NAVER_MAP_CLIENT_KEY,
   API_BASE_URL,
   VALID_CATEGORIES,
 } as const
