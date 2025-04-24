5d5e93d8c211e48497235bec61be8add571050b0
goheon <gohoney22@gmail.com>
2025-04-24 22:37:32 +0900

    feat: api 주소 변경

--- app/_lib/constants.ts
+++ app/_lib/constants.ts
@@ -6,15 +6,13 @@ const NAVER_MAP_CLIENT_ID = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID
 const NAVER_MAP_CLIENT_KEY = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_KEY
 
 const API_BASE_URL_OBJ = {
-  production:
-    'http://ec2-3-36-23-213.ap-northeast-2.compute.amazonaws.com:8080/api',
+  production: 'https://api.noongil.org:8080/api',
   development: 'http://localhost:8080/api',
   test: '',
 }
 
 const API_LOGIN_BASE_URL_OBJ = {
-  production:
-    'http://ec2-3-36-23-213.ap-northeast-2.compute.amazonaws.com:8080/oauth2',
+  production: 'https://api.noongil.org:8080/oauth2',
   development: 'http://localhost:8080/oauth2',
   test: '',
 }
