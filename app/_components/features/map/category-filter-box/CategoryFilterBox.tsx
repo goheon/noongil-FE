06ea38ef9e63e214024a10dc7622036c4001bc68
goheon <gohoney22@gmail.com>
2025-05-21 00:02:00 +0900

    feat: 카테고리 필터 컴포넌트 구조화

--- app/_components/features/map/category-filter-box/CategoryFilterBox.tsx
+++ app/_components/features/map/category-filter-box/CategoryFilterBox.tsx
@@ -6,27 +6,31 @@ import styles from './CategoryFilterBox.module.scss'
 
 const cx = classNames.bind(styles)
 
+const CATEGORY_CHIP_BUTTON_LIST = [
+  '패션',
+  '캐릭터',
+  '식음료(F&B)',
+  '뷰티',
+  'IT',
+  '라이프스타일',
+  '엔터',
+]
+
 export const CategoryFilterBox = () => {
   return (
     <div className={cx('map-category-filter-box')}>
-      <div className={cx('category-chip-button')}>
-        <p className={cx('chip-label')}>카테고리1</p>
-      </div>
-      <div className={cx('category-chip-button')}>
-        <p>카테고리1</p>
-      </div>
-      <div className={cx('category-chip-button')}>
-        <p>카테고리1</p>
-      </div>
-      <div className={cx('category-chip-button')}>
-        <p>카테고리1</p>
-      </div>
-      <div className={cx('category-chip-button')}>
-        <p>카테고리1</p>
-      </div>
-      <div className={cx('category-chip-button')}>
-        <p>카테고리1</p>
-      </div>
+      {
+        CATEGORY_CHIP_BUTTON_LIST.map((category, idx) => (
+          <CategoryChipButton category={category} key={idx} />
+        ))
+      }
     </div>
   )
+}
+
+// 사용자 선호 카테고리 버튼
+const CategoryChipButton = ({ category }: { category: string }) => {
+  return <div className={cx('category-chip-button')} >
+  <p className={cx('chip-label')}>{category}</p>
+</div>
 }
