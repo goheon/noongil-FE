599a36fb11a9df0511725f7fee940f3252bff5e4
goheon <gohoney22@gmail.com>
2025-05-21 19:15:54 +0900

    지도 필터 컴포넌트 파일 추가

--- app/(map)/map/page.tsx
+++ app/(map)/map/page.tsx
@@ -3,6 +3,7 @@ import { CategoryDial } from '@/app/_components/features/map/category-dial/Categ
 import { CurrentButton } from '@/app/_components/features/map/current-button/CurrentButton'
 import { FilterButton } from '@/app/_components/features/map/filter-button/FilterButton'
 import { CategoryFilterBox } from '@/app/_components/features/map/category-filter-box/CategoryFilterBox'
+import { FilterBox } from '@/app/_components/features/map/filter-box/FilterBox'
 
 const MapPage: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const MapPage: React.FC = () => {
       <FilterButton />
       <CurrentButton />
       <CategoryDial />
+      <FilterBox />
     </>
   )
 }
