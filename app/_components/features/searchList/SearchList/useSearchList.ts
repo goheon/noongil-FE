4b0ccac10f0278e1ab156161e0f2788b8e008484
mokyoungg <mokyoungg@gmail.com>
2025-05-22 16:51:43 +0900

    feat: create skeleton ui for search list

--- app/_components/features/searchList/SearchList/useSearchList.ts
+++ app/_components/features/searchList/SearchList/useSearchList.ts
@@ -7,18 +7,19 @@ import { TAllEventCode } from '@/app/_types'
 // TODO : list 관련 API 완성시 로직 수정
 
 const useSearchList = (eventCode: TAllEventCode) => {
-  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
-    queryKey: ['search-list'],
-    queryFn: ({ pageParam }) =>
-      getAllEventList({ pageParam, sortType: '20', eventCode: eventCode }),
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, pages) => {
-      const totalPages = lastPage.total
-      const nextPage = pages.length + 1
+  const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['search-list'],
+      queryFn: ({ pageParam }) =>
+        getAllEventList({ pageParam, sortType: '20', eventCode: eventCode }),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, pages) => {
+        const totalPages = lastPage.total
+        const nextPage = pages.length + 1
 
-      return nextPage <= totalPages ? nextPage : undefined
-    },
-  })
+        return nextPage <= totalPages ? nextPage : undefined
+      },
+    })
 
   const list = useMemo(() => {
     return data?.pages?.reduce<ISearchListItem[]>((acc, item) => {
@@ -31,6 +32,7 @@ const useSearchList = (eventCode: TAllEventCode) => {
     fetchNextPage,
     hasNextPage,
     isFetching,
+    isFetchingNextPage,
   }
 }
 
