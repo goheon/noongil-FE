c5d11eabd5d1035330f9592104555882b7b10d49
mokyoungg <mokyoungg@gmail.com>
2025-06-13 18:08:55 +0900

    feat: create custom hook for set region filter to store

---
+++ app/_components/features/searchList/useSetGeoFilterOption.ts
@@ -0,0 +1,23 @@
+import { getGeoOptions } from './searchApi'
+import { useQuery } from '@tanstack/react-query'
+import { useListFilterStore } from '@/app/_store/listFilter/useListFilterStore'
+import { useEffect } from 'react'
+
+const useSetGeoFilterOption = () => {
+  const { regionFilter, setRegionFilter } = useListFilterStore()
+
+  const { data } = useQuery({
+    queryKey: ['geo-filter'],
+    queryFn: getGeoOptions,
+    staleTime: 1000 * 60 * 10,
+    enabled: regionFilter === null,
+  })
+
+  useEffect(() => {
+    if (data) {
+      setRegionFilter(data)
+    }
+  }, [data])
+}
+
+export default useSetGeoFilterOption
