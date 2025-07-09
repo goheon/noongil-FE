4c6ae5351f48c269a8998478c6ce9b0c26b772cd
mokyoungg <mokyoungg@gmail.com>
2025-07-09 20:09:12 +0900

    feat: change popularlist img size

--- app/_components/features/main/PopularList/PopularListItem.tsx
+++ app/_components/features/main/PopularList/PopularListItem.tsx
@@ -8,16 +8,18 @@ import { formatDateRange } from '@/app/_utils/textFormatter'
 import { useMemo } from 'react'
 import { getEventDetailUrl } from '@/app/_utils/navigation'
 import Link from 'next/link'
+import { TEventCodeName } from '@/app/_types'
 
 const cx = classNames.bind(styles)
 
 interface PopularListItemProps {
   data: IListItem
   idx: number
+  eventCode: TEventCodeName
 }
 
 const PopularListItem = (props: PopularListItemProps) => {
-  const { data, idx } = props
+  const { data, idx, eventCode } = props
 
   const date = useMemo(
     () => formatDateRange(data.operStatDt, data.operEndDt),
@@ -31,20 +33,26 @@ const PopularListItem = (props: PopularListItemProps) => {
   return (
     <Link href={detailUrl}>
       <div className={cx('list-item')}>
-        <Image
-          className={cx('image')}
-          src={data.imageUrl || SampleImage}
-          alt="image"
-          width={200}
-          height={200}
-        />
-        <div className={cx('info-section')}>
-          <div className={cx('rank')}>{idx + 1}</div>
+        <div className={cx('img-wrapper')}>
+          <Image
+            className={cx('image')}
+            src={data.imageUrl || SampleImage}
+            alt="image"
+            fill
+          />
+        </div>
 
-          <div className={cx('info')}>
-            <div className={cx('title')}>{data.eventNm}</div>
-            <div className={cx('event-date')}>{date}</div>
-          </div>
+        <div className={cx('info')}>
+          <div className={cx('title')}>{data.eventNm}</div>
+          <div className={cx('event-date')}>{date}</div>
+        </div>
+
+        <div
+          className={cx('rank', {
+            'rank--exhibition': eventCode === 'exhibition',
+          })}
+        >
+          {idx + 1}
         </div>
       </div>
     </Link>
