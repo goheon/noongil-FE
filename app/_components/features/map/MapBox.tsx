bc00a6cba77ad73a6c4ce8d88382c4da2f43a54a
goheon <gohoney22@gmail.com>
2025-08-17 18:17:19 +0900

    fix: 지도 마커 클릭 줌 효과 비활성화

--- app/_components/features/map/MapBox.tsx
+++ app/_components/features/map/MapBox.tsx
@@ -181,7 +181,12 @@ export const MapBox: React.FC = () => {
       })
       map.setZoom(16, true)
     } else {
-      map.fitBounds(bounds)
+      map.fitBounds(bounds, {
+        top: 50,
+        bottom: 50,
+        left: 50,
+        right: 50,
+      })
       map.panToBounds(
         bounds,
         {},
