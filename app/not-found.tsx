a9c5b1dbfb1be712ec96993d6077ea016c4e005e
goheon <gohoney22@gmail.com>
2025-09-15 10:58:16 +0900

    style: not-found 페이지 문구 수정, 스타일 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link'
+import classNames from 'classnames/bind'
+import styles from './not-found.module.scss'
+
+const cx = classNames.bind(styles)
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className={cx('not-found')}>
+      <h2 className={cx('title')}>
+        요청하신 페이지를 <br /> 찾지 못했어요.
+      </h2>
+      <p className={cx('content')}>다른 페이지를 탐색해볼까요?</p>
+      <Link className={cx('home')} href="/">
+        홈으로 돌아가기
+      </Link>
     </div>
   )
 }
